6fbe6d72e6ac4445adb4951e2064214e0678e7ba
Peter Ballok <peter.ballok@logmein.com>
2023-02-01 18:26:59 -0500

    create first structure

---
+++ backend/go.mod
@@ -0,0 +1,3 @@
+module github.com/pballok/gw2-crafting-helper/backend
+
+go 1.19
---
+++ backend/internal/item/item.go
@@ -0,0 +1,10 @@
+package item
+
+type Item struct {
+	Id   string
+	Name string
+}
+
+func FetchItem(id string) (*Item, error) {
+	return nil, nil
+}
