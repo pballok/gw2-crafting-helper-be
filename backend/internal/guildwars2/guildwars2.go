4602c0be1c72ea1d20d08c0dbb6569f5c6e7cf7d
Peter Ballok <pballok@gmail.com>
2023-03-17 12:07:46 -0400

    Item price (#2)
    * fetch prices of an item
    
    * return with full json

--- backend/internal/guildwars2/guildwars2.go
+++ backend/internal/guildwars2/guildwars2.go
@@ -11,27 +11,63 @@ const api_base = "https://api.guildwars2.com/v2"
 type Item struct {
 	Id   int    `json:"id"`
 	Name string `json:"name"`
+	Icon string `json:"icon"`
 }
 
-func FetchItem(id int) (*Item, error) {
-	resp, err := http.Get(fmt.Sprintf("%s/items/%d", api_base, id))
+type Prices struct {
+	Id   int `json:"id"`
+	Buys struct {
+		Price int `json:"unit_price"`
+	} `json:"buys"`
+	Sells struct {
+		Price int `json:"unit_price"`
+	} `json:"sells"`
+}
+
+type resultType interface{}
+
+func fetch[R resultType](url string) (*R, error) {
+	var result R
+	resp, err := http.Get(url)
 	if err != nil {
-		return nil, fmt.Errorf("failed to fetch item %d : %w", id, err)
+		return &result, err
 	}
 	defer resp.Body.Close()
 
 	if resp.StatusCode == http.StatusNotFound {
-		return &Item{}, nil
+		return &result, nil
 	}
+
 	if resp.StatusCode != http.StatusOK {
-		return nil, fmt.Errorf("failed to fetch item %d from guildwars2: API returned with %d", id, resp.StatusCode)
+		return &result, fmt.Errorf("failed to fetch from guildwars2: API returned with %d", resp.StatusCode)
 	}
 
-	item := Item{}
-	err = json.NewDecoder(resp.Body).Decode(&item)
+	err = json.NewDecoder(resp.Body).Decode(&result)
 	if err != nil {
-		return nil, fmt.Errorf("failed to decode item %d from response : %w", id, err)
+		return &result, fmt.Errorf("failed to decode from response : %w", err)
 	}
 
-	return &item, nil
+	return &result, nil
+}
+
+func FetchItem(id int) (*Item, error) {
+	requestUrl := fmt.Sprintf("%s/items/%d", api_base, id)
+	item, err := fetch[Item](requestUrl)
+
+	if err != nil {
+		return nil, fmt.Errorf("failed to fetch item %d : %w", id, err)
+	}
+
+	return item, nil
+}
+
+func FetchPrices(id int) (*Prices, error) {
+	requestUrl := fmt.Sprintf("%s/commerce/prices/%d", api_base, id)
+	prices, err := fetch[Prices](requestUrl)
+
+	if err != nil {
+		return nil, fmt.Errorf("failed to fetch prices for item %d : %w", id, err)
+	}
+
+	return prices, nil
 }
--- backend/internal/item/item.go
+++ backend/internal/item/item.go
@@ -3,18 +3,51 @@ package item
 import "github.com/pballok/gw2-crafting-helper/backend/internal/guildwars2"
 
 type Item struct {
-	Id   int
-	Name string
+	Id    int
+	Name  string
+	Icon  string
+	Price struct {
+		Buy  int
+		Sell int
+	}
 }
 
 func NewItem(id int) (*Item, error) {
-	item, err := guildwars2.FetchItem(id)
+	item := Item{Id: id}
+
+	err := item.fetchBase()
 	if err != nil {
 		return nil, err
 	}
 
-	return &Item{
-		Id:   item.Id,
-		Name: item.Name,
-	}, nil
+	err = item.fetchPrices()
+	if err != nil {
+		return nil, err
+	}
+
+	return &item, nil
+}
+
+func (i *Item) fetchBase() error {
+	item, err := guildwars2.FetchItem(i.Id)
+	if err != nil {
+		return err
+	}
+
+	i.Name = item.Name
+	i.Icon = item.Icon
+
+	return nil
+}
+
+func (i *Item) fetchPrices() error {
+	prices, err := guildwars2.FetchPrices(i.Id)
+	if err != nil {
+		return err
+	}
+
+	i.Price.Buy = prices.Buys.Price
+	i.Price.Sell = prices.Sells.Price
+
+	return nil
 }
